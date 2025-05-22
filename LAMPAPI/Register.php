1032ed1ec1c415cc1bb08cfbe4a6ee4333b5034b
Haresh Palli <realhareshpalli@gmail.com>
2025-05-22 16:30:51 -0400

    added header lines to test register

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -1,4 +1,13 @@
 <?php
+	// FOR LOGIN TESTING ONLY REMOVE AFTER
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+	header("Access-Control-Allow-Headers: Content-Type");
+
+	if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+		http_response_code(204);
+		exit;
+	}
 
 	$inData = getRequestInfo();
 
