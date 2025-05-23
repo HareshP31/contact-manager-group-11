8d0eba33ed0463ce02ca00dfabe88c36598606d4
Sebassoto <sebassotomayor1029@gmail.com>
2025-05-23 17:41:56 -0400

    Testing addContacts()

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,4 +1,12 @@
 <?php
+        // REMOVE AFTER TESTING
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Allow-Methods: POST, OPTIONS");
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
     //  Get incoming JSON data
     $inData = getRequestInfo();
 
