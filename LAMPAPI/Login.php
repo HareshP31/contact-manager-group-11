c8a03120282f306aa1dec80da2f33f2a9ceba718
Haresh Palli <realhareshpalli@gmail.com>
2025-05-30 20:55:01 -0400

    edited login endpoint again

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -24,7 +24,7 @@
 	else
 	{
 		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
+		$stmt->bind_param("s", $inData["login"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
